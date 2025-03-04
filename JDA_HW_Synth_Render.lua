0959de4f86b8da41d21703e70fc5cd63a974d118
Jake Kantzer <jkantzer@outlook.com>
2025-03-03 20:46:02 -0500

    Changed ReaInsert render script name and made render 2 the main one

--- JDA_HW_Synth_Render.lua
+++ JDA_HW_Synth_Render.lua
@@ -2,15 +2,13 @@
   @author Jake d'Arc
   @license MIT
   @version 1.0.0
-  @about Automatic real-time bouncing and FX chain copying for tracks using ReaInsert with synths
+  @about Real-time pre-FX rendering and FX chain copying for MIDI/audio track pairs, intended for hardware synths
   @changelog
     - init
   @provides
     [main] JDA_HW_Synth_Render/JDA_HW_Synth_Render_1P.lua
     [main] JDA_HW_Synth_Render/JDA_HW_Synth_Render_2P.lua
 ]]
-
-dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
 
 function bounce(second_pass)
   -- Render to new track
@@ -22,6 +20,7 @@ function bounce(second_pass)
 end
 
 function main(second_pass)
+  reaper.PreventUIRefresh(1)
   reaper.Undo_BeginBlock()
 
   -- If we didn't end up with a track, check if one is selected already, otherwise bail
@@ -32,8 +31,52 @@ function main(second_pass)
 
   local orig_track = reaper.GetSelectedTrack(0, 0)
   if orig_track == nil then
-    reaper.ShowMessageBox("Please select a track.", "Error", 0)
+    reaper.ShowMessageBox("Please select a single track.", "Error", 0)
   return end
+
+  -- Time to check that they selected a track beginning in either "M: " or "A: "
+  local retval, first_track_name = reaper.GetTrackName(orig_track)
+
+  local audio_track = nil
+  local midi_track = nil
+
+  -- If they selected the MIDI track, we need to find the corresponding audio one or vice-versa
+  if string.sub(first_track_name, 0, 3) == "M: " then
+    midi_track = orig_track
+    local track_count = reaper.CountTracks(0)
+    for i = 0, track_count - 1 do
+      local current_track = reaper.GetTrack(0, i)
+      local retval, second_track_name = reaper.GetTrackName(current_track)
+
+      if string.sub(second_track_name, 0, 3) == "A: " then
+        if (string.sub(second_track_name, 4) == string.sub(first_track_name, 4)) then
+          audio_track = current_track
+          break
+        end
+      end
+    end
+  elseif string.sub(first_track_name, 0, 3) == "A: " then
+    audio_track = orig_track
+    local track_count = reaper.CountTracks(0)
+    for i = 0, track_count - 1 do
+      local current_track = reaper.GetTrack(0, i)
+      local retval, second_track_name = reaper.GetTrackName(current_track)
+
+      if string.sub(second_track_name, 0, 3) == "M: " then
+        if (string.sub(second_track_name, 4) == string.sub(first_track_name, 4)) then
+          midi_track = current_track
+          break
+        end
+      end
+    end
+  end
+
+  if audio_track == nil or midi_track == nil then
+    reaper.ShowMessageBox("Could not find matching MIDI/audio pair. Please name MIDI tracks beginning with \"M: \" and audio tracks beginning with \"A: \"", "Error", 0)
+  return end
+
+  -- Select only the MIDI track so we can get its items
+  reaper.SetOnlyTrackSelected(midi_track)
 
   -- Deselect all items first
   reaper.Main_OnCommand(40289, 0)
@@ -54,15 +97,12 @@ function main(second_pass)
     reaper.ShowMessageBox("There is no loop set, aborting.", "Error", 0)
   return end
 
-  -- Cut early if first effect isn't ReaInsert
-  local first_fx, first_fx_name = reaper.TrackFX_GetFXName(orig_track, 0)
-  if not first_fx or first_fx_name ~= "VST: ReaInsert (Cockos)" then
-    reaper.ShowMessageBox("The first effect on this track is not ReaInsert, aborting.", "Error", 0)
-  return end
+  -- Move on to the audio track to do work there
+  reaper.SetOnlyTrackSelected(audio_track)
 
   -- Get the automation mode, store it, and set it to trim/read so effects bypass correctly
-  local orig_automation_mode = reaper.GetTrackAutomationMode(orig_track)
-  reaper.SetTrackAutomationMode(orig_track, 0)
+  local orig_automation_mode = reaper.GetTrackAutomationMode(audio_track)
+  reaper.SetTrackAutomationMode(audio_track, 0)
 
   -- Store the original render speed
   store_render_speed_id = reaper.NamedCommandLookup("_XENAKIOS_STORERENDERSPEED")
@@ -76,12 +116,12 @@ function main(second_pass)
   if set_render_speed_rt_id == nil then return end
   reaper.Main_OnCommand(set_render_speed_rt_id, 0)
 
-  -- Store the original bypass states and bypass all but the first effect
+  -- Store the original bypass states and bypass all
   local fx_bypass_states = {}
-  local num_fx = reaper.TrackFX_GetCount(orig_track)
+  local num_fx = reaper.TrackFX_GetCount(audio_track)
   for i = 0, num_fx - 1 do
-      fx_bypass_states[i] = reaper.TrackFX_GetEnabled(orig_track, i)
-      if i ~= 0 then reaper.TrackFX_SetEnabled(orig_track, i, false) end
+      fx_bypass_states[i] = reaper.TrackFX_GetEnabled(audio_track, i)
+      reaper.TrackFX_SetEnabled(audio_track, i, false)
   end
 
   -- Render to new track
@@ -96,36 +136,45 @@ function main(second_pass)
   local new_track = reaper.GetSelectedTrack(0, 0)
 
   -- Get the number of FX in the source track
-  local num_fx = reaper.TrackFX_GetCount(orig_track)
+  local num_fx = reaper.TrackFX_GetCount(audio_track)
 
   -- Restore the original bypass states before copying effects
   for i, state in pairs(fx_bypass_states) do
-      reaper.TrackFX_SetEnabled(orig_track, i, state)
+      reaper.TrackFX_SetEnabled(audio_track, i, state)
   end
   
-  -- Get FX chunk from original track and send to new track (ultraschall needed for envelopes)
-  local orig_tsc_returned, orig_track_state_chunk = reaper.GetTrackStateChunk(orig_track, "", false)
-  local new_tsc_returned, new_track_state_chunk = reaper.GetTrackStateChunk(new_track, "", false)
-  local orig_track_fxsc = ultraschall.GetFXStateChunk(orig_track_state_chunk)
-  local new_fxsc_returned, new_modified_track_state_chunk = ultraschall.SetFXStateChunk(new_track_state_chunk, orig_track_fxsc)
-  reaper.SetTrackStateChunk(new_track, new_modified_track_state_chunk, false)
-
-  -- Delete ReaInsert
-  reaper.TrackFX_Delete(new_track, 0)
-
-  -- Unmute the original track
-  reaper.SetMediaTrackInfo_Value(orig_track, "B_MUTE", 0)
+  -- Copy all FX
+  for i = 0, num_fx - 1 do
+    local fx_chunk = reaper.TrackFX_GetFXGUID(audio_track, i)
+    reaper.TrackFX_CopyToTrack(audio_track, i, new_track, reaper.TrackFX_GetCount(new_track), false)
+  end
 
   -- Restore the original automation mode
-  reaper.SetTrackAutomationMode(orig_track, orig_automation_mode)
+  reaper.SetTrackAutomationMode(audio_track, orig_automation_mode)
 
-  -- Mute the items on the original track
+  -- Mute the items from the original MIDI track
   for i = 1, #selected_items do
       reaper.SetMediaItemInfo_Value(selected_items[i], "B_MUTE", 1)
   end
 
-  -- Update the arrange view to reflect the changes
+  -- Unmute the original audio track because the render mutes it
+  reaper.SetMediaTrackInfo_Value(audio_track, "B_MUTE", 0)
+
+  -- Move the audio track above the new track because the new track is made above it for some reason
+  reaper.SetOnlyTrackSelected(audio_track)
+  local new_track_id = math.floor(reaper.GetMediaTrackInfo_Value(new_track, "IP_TRACKNUMBER"))
+  reaper.ReorderSelectedTracks(new_track_id - 1, 0)
+
+  -- Color and name
+  reaper.SetTrackColor(new_track, reaper.GetTrackColor(audio_track))
+  local retval, audio_track_name = reaper.GetTrackName(audio_track)
+  local new_track_name = "S: " .. string.sub(audio_track_name, 4)
+  reaper.GetSetMediaTrackInfo_String(new_track, "P_NAME", new_track_name, true)
+
+  -- Wrap up
+  reaper.SetOnlyTrackSelected(new_track)
   reaper.UpdateArrange()
+  reaper.PreventUIRefresh(-1)
 
   reaper.Undo_EndBlock('Hardware synth render',-1)
 end
---
+++ JDA_HW_Synth_Render_RI/JDA_HW_Synth_Render_1P_RI.lua
@@ -0,0 +1,2 @@
+dofile(reaper.GetResourcePath().."/Scripts/JakeDArc/JDA_HW_Synth_Render_RI.lua")
+main(false)
